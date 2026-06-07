1a7c313c800228dbad7ad6082500b422cae0800f
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-06-07 19:17:27 +0530

    Update StatsScreen.kt

--- app/src/main/java/com/jay/m3play/ui/screens/StatsScreen.kt
+++ app/src/main/java/com/jay/m3play/ui/screens/StatsScreen.kt
@@ -54,7 +54,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.jay.innertube.models.WatchEndpoint
 import com.jay.m3play.LocalPlayerAwareWindowInsets
