c9779c9a0d5df6dbbb3c14e9e735f3d12e285d3b
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-06-07 20:06:40 +0530

    Update OnlineSearchScreen.kt

--- app/src/main/java/com/jay/m3play/ui/screens/search/OnlineSearchScreen.kt
+++ app/src/main/java/com/jay/m3play/ui/screens/search/OnlineSearchScreen.kt
@@ -44,7 +44,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.jay.innertube.models.AlbumItem
 import com.jay.innertube.models.ArtistItem
